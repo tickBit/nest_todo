d70fde30de79ee278ca8284b21c87d16e7cf2885
tickBit <marjukosk@gmail.com>
2026-04-12 12:21:50 +0300

    Move .husky to root of the project

---
+++ backend/test/app.e2e-spec.ts
@@ -0,0 +1,7 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+import { describe } from 'node:test';
+
