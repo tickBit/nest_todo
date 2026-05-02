d8fc453c2d0b9a42ab8c71864b30b2c0a857ef29
tickBit <marjukosk@gmail.com>
2026-05-02 20:23:29 +0300

    Handle error messages

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
   const navigate = useNavigate();
 
+  const [messages, setMessages] = useState<string[]>([]);
+  
   const handleSignIn = async(e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -11,22 +14,35 @@ const SignIn = () => {
     const password = formData.get('password') as string;
     if (!email || !password) return;
 
-    await fetch('/auth/login', {
+    try {
+      const response = await fetch('/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password }),
-        }).then(response => {
-          if (!response.ok) throw new Error(`HTTP ${response.status}`);
-          return response.json();
         })
-        .then(data => {
-          console.log('User logged in:', data);
-          localStorage.setItem('access_token', data.access_token);
-          navigate('/');
-        })
-        .catch(error => console.error('Error logging in user:', error));
+          
+        const data = await response.json();
+      
+        if (!response.ok) {
+          setMessages([]);
+          console.error('SignIn failed:', response.status, data);
+          
+          setMessages([data.message]);
+          
+          return;
+        }
+        
+        setMessages([]);
+        console.log('User registered:', data);
+        localStorage.setItem('access_token', data.access_token);
+        navigate('/');
+            
+      } catch (error) {
+        console.error('Error registering user:', error);
+      }
+        
   }
   
   return (
@@ -41,6 +57,17 @@ const SignIn = () => {
             <input type="password" name="password" placeholder="Password" className="input" />
             <button type="submit" className="btn bg-blue-500 text-white">Login</button>
         </form>
+        </div>
+        <div className="error-container">
+        {messages.length > 0 ? (
+          <>
+            <h2 className="text-xl text-red-500">Errors:</h2>
+              {messages.map((msg, index) => (
+                <p key={index} className="text-red-500">{msg}</p> 
+            
+              ))}
+          </>
+         ) : null}
         </div>
     </div>
   )
