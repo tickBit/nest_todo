190d2d1e74b1c945ccfc554f14a72acd6b34d374
tickBit <marjukosk@gmail.com>
2026-05-02 12:53:55 +0300

    Add routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import SignUp from './components/SignUp.tsx';
+import SignIn from './components/SignIn.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/register" element={<SignUp  />} />
+            <Route path="/login" element={<SignIn />} />
+          </Routes>
+      </Router>
   </StrictMode>,
 )
