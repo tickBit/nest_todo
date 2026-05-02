9cce47f0eba4455596ac32f3e8c3805bb9f118dc
tickBit <marjukosk@gmail.com>
2026-05-02 07:45:24 +0300

    Fix Todo component

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -3,7 +3,7 @@ import type { Task } from '../types';
 const Todo = (props: Task) => {
   return (
     <div className="todo">
-        <h3>{props.title}</h3>
+        <h3>{props.todoName}</h3>
         <p>{props.done ? "Done" : "Not Done"}</p>
     </div>
   )
