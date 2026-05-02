cbc8e51f100a058061d1a5d4f3c92b0175b899d6
tickBit <marjukosk@gmail.com>
2026-05-02 13:51:45 +0300

    Add logout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,6 +12,12 @@ const Header = () => {
     navigate('/login');
   }
   
+  const handleLogout = () => {
+    localStorage.removeItem('access_token');
+    navigate('/');
+    history.go(0);
+  }
+  
   return (
     <header className="bg-gray-700 p-4">
         <h1 className="text-2xl text-yellow-400 font-bold">My Todo App</h1>
@@ -19,6 +25,7 @@ const Header = () => {
         <div className="absolute top-4 right-4">
           <button className="btn rounded-full bg-green-500 text-white text-sm" onClick={ handleSignUp }>Sign Up</button>
           <button className="btn rounded-full bg-blue-500 text-white text-sm" onClick={ handleSignIn }>Login</button>
+          <button className="btn rounded-full bg-red-500 text-white text-sm" onClick={ handleLogout }>Logout</button>
         </div>
     </header>
     )
