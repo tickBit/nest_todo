386af0664aefa626db2a4f1345ee58feea88dacf
tickBit <marjukosk@gmail.com>
2026-07-31 22:42:35 +0300

    Add input box for passwordless login

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,30 @@
+
 import { useNavigate } from "react-router-dom";
 
-const Header = () => {       
-
+const Header = () => {         
   const navigate = useNavigate();
 
+  const getLink = () => {
+    
+    // call the backend endpoint to get the login link
+    fetch('http://localhost:3000/auth/login-with-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: (document.querySelector('input[type="text"]') as HTMLInputElement).value })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Magic link sent:', data);
+            
+      alert(data.message);
+    }
+    )
+    .catch(error => console.error('Error sending magic link:', error));
+  }
+  
+  
   const handleSignUp = () => {
     navigate('/register');
   }
@@ -21,6 +42,11 @@ const Header = () => {
   return (
     <header className="bg-gray-700 p-4">
         <h1 className="text-2xl text-yellow-400 font-bold">My Todo App</h1>
+        
+        <div>
+          <button className="btn rounded-full bg-purple-500 text-white text-sm" onClick={ getLink }>Get Magic Link</button>
+          <input className="email" type="text" placeholder="Enter your email" />
+        </div>
         
         <div className="absolute top-4 right-4">
           <button className="btn rounded-full bg-green-500 text-white text-sm" onClick={ handleSignUp }>Sign Up</button>
