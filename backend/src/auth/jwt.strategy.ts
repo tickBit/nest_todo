f477e1d038c26eba629d6204d2c829afb79bbdab
tickBit <marjukosk@gmail.com>
2026-08-01 21:38:41 +0300

    Add default value for JWT key

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET!,
+      secretOrKey: process.env.JWT_SECRET! || 'my_secret_key',
     });
   }
 
